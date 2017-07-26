b8bcfbb7eff229228b46db1e3de1862f5fe911ea
Tomas <lightcalamar@gmail.com>
2017-07-26 20:34:54 +0200

    Add files via upload

---
+++ 1.Basics/Helloworld/Helloworld.ino
@@ -0,0 +1,16 @@
+//#include <EEPROM.h> 
+#include <SPI.h>
+#include <GD23STM32.h>
+
+void setup()
+{
+  GD.begin();
+}
+
+void loop()
+{
+  GD.ClearColorRGB(0x002000);
+  GD.Clear();
+  GD.cmd_text(GD.w / 2, GD.h / 2, 31, OPT_CENTER, "Hello world");
+  GD.swap();
+}
