b35101e492ad1ffa1d0c8a573db6d2f33ccd422d
Tomas <lightcalamar@gmail.com>
2017-08-13 17:10:51 -0500

    Update Parametros.ino

--- 2.Images/STM32F103_Asset_y_JPG/Parametros.ino
+++ 2.Images/STM32F103_Asset_y_JPG/Parametros.ino
@@ -3,5 +3,5 @@ char TXP[50];
 void STM32()
 {
   GD.ColorRGB(0x00ff00); sprintf(TXP,"F_CPU: %d MHz", (F_CPU/1000000));  GD.cmd_text(GD.w - 105, 0, 26, 0, TXP);
-  GD.ColorRGB(0x00ff00); sprintf(TXP,"F_SDIO:  %d MHz", (FreqSDIO/1000000));  GD.cmd_text(GD.w - 105, 25, 26, 0, TXP);
+  GD.ColorRGB(0x00ff00); sprintf(TXP,"F_SDIO:  %d MHz", (FreqSDIO/1000000));  GD.cmd_text(GD.w - 105, 20, 26, 0, TXP);
 }
