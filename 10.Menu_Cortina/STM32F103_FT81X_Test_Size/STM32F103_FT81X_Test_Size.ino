6da33f0aa94ea347d55962d2011fe3edb3aa3cf3
Tomas <lightcalamar@gmail.com>
2017-08-13 17:38:39 -0500

    Update STM32F103_FT81X_Test_Size.ino

--- 10.Menu_Cortina/STM32F103_FT81X_Test_Size/STM32F103_FT81X_Test_Size.ino
+++ 10.Menu_Cortina/STM32F103_FT81X_Test_Size/STM32F103_FT81X_Test_Size.ino
@@ -4,7 +4,6 @@
 
 void setup() {
  GD.begin();
-
 }
 
 void loop() {
@@ -21,5 +20,14 @@ void loop() {
     GD.Vertex2f(1 * 16, 1* 16);
     GD.Vertex2f((GD.w-2) * 16, (GD.h-2) * 16);
  
+  STM32();
   GD.swap();
+}
+
+char TXP[50];
+
+void STM32()
+{
+  GD.ColorRGB(0x00ff00); sprintf(TXP,"F_CPU: %d MHz", (F_CPU/1000000));  GD.cmd_text(GD.w - 105, 0, 26, 0, TXP);
+  GD.ColorRGB(0x00ff00); sprintf(TXP,"F_SDIO:  %d MHz", (FreqSDIO/1000000));  GD.cmd_text(GD.w - 105, 25, 26, 0, TXP);
 }
